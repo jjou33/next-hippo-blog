6270bea155fb298eb13d0fa06e108f90a577628d
jjou33 <zanda33@naver.com>
2023-08-14 23:31:23 +0900

    FEAT: Counter Animation Hook 컴포넌트 개발 및 스타일 수정
    - 카운터 훅 개발 완료
    - 기타 스타일 수정

---
+++ components/common/NumberCount/index.tsx
@@ -0,0 +1,25 @@
+import { Divider } from '../StyledLayout'
+import theme from 'styles/theme'
+import * as S from './styles'
+import useNumberCount from 'hooks/useNumberCount'
+import Typography from '../Typography'
+const NumberCount = ({ number, name, duration }) => {
+  return (
+    <S.CountContainer>
+      <S.CountNameBox>
+        <Typography variant="span" aggressive="tab_003">
+          {name}
+        </Typography>
+      </S.CountNameBox>
+      <Divider
+        direction="horizontal"
+        width="100%"
+        height="1px"
+        color={theme.colors.gray_002}
+      />
+      {useNumberCount({ number, duration })}
+    </S.CountContainer>
+  )
+}
+
+export default NumberCount
---
+++ components/common/NumberCount/styles.ts
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled'
+
+export const CountContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  justify-content: center;
+  width: 70px;
+  height: 60px;
+  border: 3px solid black;
+  border-radius: 10px;
+`
+
+export const CountNameBox = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 70px;
+  font-size: 0.5rem;
+  padding: 0 5px;
+`
+
+export const CountNumberBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
+  justify-content: center;
+  width: 70px;
+  height: 60px;
+  background-color: white;
+  border-radius: 10px;
+`
