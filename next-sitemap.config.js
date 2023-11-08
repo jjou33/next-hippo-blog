bc75588dd91c235f6604ffb19e71ad6e282e0213
jjou33 <zanda33@naver.com>
2023-11-08 15:57:18 +0900

    [#11] SEO: Site Domain URL 변경에 따른 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: 'https://next-hippo-blog.vercel.app/',
+  siteUrl: 'https://hippodevlog.vercel.app/',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'daily',
