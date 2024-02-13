9ae37623759873d6da63f8657e99e9d0097ee507
jjou33 <zanda33@naver.com>
2024-02-13 13:39:13 +0900

    [#dev] SITEMAP CONFIG 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  siteUrl: 'https://next-hippo-blog.vercel.app/',
+  siteUrl: 'https://hippodevelop.vercel.app/',
   generateIndexSitemap: false,
   generateRobotsTxt: true,
   sitemap: {
     additionalSitemaps: [], // 추가적인 Sitemap 파일이 생성되지 않도록 빈 배열 설정
     sitemapSize: 50000,
   },
-  
+
   changefreq: 'daily',
   priority: 1,
   robotsTxtOptions: {
