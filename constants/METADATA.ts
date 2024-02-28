42dda892d03c15730e607c319d264e408ae5bb9c
jjou33 <zanda33@naver.com>
2024-02-28 23:36:21 +0900

    [#dev] SEO: Blog Domain Change

--- constants/METADATA.ts
+++ constants/METADATA.ts
@@ -3,8 +3,8 @@ const METADATA = {
   meta: {
     title: 'HIPPO DEV',
     description: 'HIPPO DEV 의 개발 일기장에 오신것을 환영합니다!',
-    url: 'https://hippodevelop.vercel.app',
-    imageUrl: 'https://hippodevelop.vercel.app/static/images',
+    url: 'https://hippoblog.vercel.app',
+    imageUrl: 'https://hippoblog.vercel.app/static/images',
   },
 } as const
 
