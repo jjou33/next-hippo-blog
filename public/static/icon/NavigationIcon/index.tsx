c27a610587e4b4bf43bcf4376164f3427461c795
happycho <zanda33@naver.com>
2024-09-20 10:10:14 +0900

    [#117] DOCS: monorepo DOCS

--- public/static/icon/NavigationIcon/index.tsx
+++ public/static/icon/NavigationIcon/index.tsx
@@ -1,6 +1,38 @@
 import type { IconPropsType } from 'public/static/icon/index'
 
 export const NavigationIconSet: IconPropsType = {
+  monorepo: {
+    icon: () => (
+      <svg
+        width={'100%'}
+        height={'100%'}
+        fill={'#93a1ae'}
+        viewBox={'0 0 24 24'}
+      >
+        <g id={'SVGRepo_bgCarrier'} strokeWidth={'0'} />
+        <g
+          id={'SVGRepo_tracerCarrier'}
+          strokeLinecap={'round'}
+          strokeLinejoin={'round'}
+        />
+        <g id={'SVGRepo_iconCarrier'}>
+          <path
+            fillRule={'evenodd'}
+            d={
+              'M12 21a1.75 1.75 0 110-3.5 1.75 1.75 0 010 3.5zm-3.25-1.75a3.25 3.25 0 106.5 0 3.25 3.25 0 00-6.5 0zm-3-12.75a1.75 1.75 0 110-3.5 1.75 1.75 0 010 3.5zM2.5 4.75a3.25 3.25 0 106.5 0 3.25 3.25 0 00-6.5 0zM18.25 6.5a1.75 1.75 0 110-3.5 1.75 1.75 0 010 3.5zM15 4.75a3.25 3.25 0 106.5 0 3.25 3.25 0 00-6.5 0z'
+            }
+          />
+          <path
+            fillRule={'evenodd'}
+            d={
+              'M6.5 7.75v1A2.25 2.25 0 008.75 11h6.5a2.25 2.25 0 002.25-2.25v-1H19v1a3.75 3.75 0 01-3.75 3.75h-6.5A3.75 3.75 0 015 8.75v-1h1.5z'
+            }
+          />
+          <path fillRule={'evenodd'} d={'M11.25 16.25v-5h1.5v5h-1.5z'} />
+        </g>
+      </svg>
+    ),
+  },
   loadtest: {
     icon: () => (
       <svg width={'100%'} height={'100%'} viewBox={'0 0 24 24'} fill={'none'}>
@@ -858,7 +890,7 @@ export const NavigationIconSet: IconPropsType = {
       </svg>
     ),
   },
-  Design: {
+  design: {
     icon: (color: string) => (
       <svg fill={color} width={'100%'} height={'100%'} viewBox={'0 0 512 512'}>
         <g id={'SVGRepo_bgCarrier'} strokeWidth={'0'} />
